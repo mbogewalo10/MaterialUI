a6d8e5813a96ec55b16c922215da26ab4d002697
assanmboge <mbogeassan77@gmail.com>
2020-01-09 05:44:22 +0200

    Updates

---
+++ precache-manifest.1f67033751defb56f95dafbc2161be58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b32d712e711ad744755a583cc4fdf9",
+    "url": "/materialUi/index.html"
+  },
+  {
+    "revision": "33bdc18aa05dceab2df7",
+    "url": "/materialUi/static/css/main.e169efa4.chunk.css"
+  },
+  {
+    "revision": "2be4f0a63732cda1ee8f",
+    "url": "/materialUi/static/js/2.e87b8ebf.chunk.js"
+  },
+  {
+    "revision": "33bdc18aa05dceab2df7",
+    "url": "/materialUi/static/js/main.84b09f8f.chunk.js"
+  },
+  {
+    "revision": "d176c04ffa6b3756d9cc",
+    "url": "/materialUi/static/js/runtime~main.2ab4b64e.js"
+  }
+]);
